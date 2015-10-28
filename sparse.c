d8b6d5839a9f95a6abed77e62681b39fd4d28f48
ujwalp15 <ujwalp09@gmail.com>
2015-10-28 19:27:18 +0530

    Program Added: Sparse Matrix

---
+++ sparse.c
@@ -0,0 +1,54 @@
+/**
+Lab Program 9
+Sparse Martix Search
+Author ujwalp15
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+#define SIZE 10
+
+typedef struct SPARSE
+{
+    int row;
+    int column;
+    int value;
+}sparse;
+
+int main()
+{
+    sparse E[SIZE];
+    int n, i, key, flag=0;
+
+
+    printf("\nEnter the Number of Non-Zero Elements in the Sparse Matrix: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    for(i = 0; i < n; i++)
+    {   printf("\n Enter the details :\n");
+	printf("\n Row Index: ");
+        scanf("%d", &E[i].row);
+	printf("\n Column Index: ");        
+        scanf("%d", &E[i].column);
+	printf("\n Value: ");       
+        scanf("%d", &E[i].value);
+    }
+
+    printf("\n\nEnter the element to search for : ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++)
+        if(E[i].value == key)
+        {
+            printf("\nThe element %d is Found At Row %d And Column %d", key, E[i].row, E[i].column);
+            flag=1;
+	    break;
+        }
+
+    if(flag == 0)
+        printf("\nElement Not Found\n");
+
+	return 0;
+}
